978315889127d002b3144a385ad3d961dbed1927
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-08 04:57:40 +0100

    fc

---
+++ src/main/java/net/axel/majesticcup/MajesticcupApplication.java
@@ -0,0 +1,13 @@
+package net.axel.majesticcup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajesticcupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajesticcupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/axel/majesticcup/MajesticcupApplicationTests.java
@@ -0,0 +1,13 @@
+package net.axel.majesticcup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajesticcupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
